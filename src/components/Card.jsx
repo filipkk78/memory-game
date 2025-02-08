d28d69b4dcd6b388297e51153f3fd0d78b4ebb15
filipkk78 <filipkk78@gmail.com>
2025-02-08 15:07:24 +0100

    Fix key bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,19 @@ import { useState, useEffect } from "react";
 
 function Card({ pokeName }) {
   const [pokemon, setPokemon] = useState(null);
+  const [pokeKey, setPokeKey] = useState(null);
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
       .then((response) => response.json())
-      .then((json) => setPokemon(json.sprites.front_default))
+      .then((json) => {
+        setPokemon(json.sprites.front_default);
+        setPokeKey(json.id);
+      })
       .catch((error) => console.error(error));
-  }, []);
-
-  console.log(pokemon);
+  }, [pokeName]);
 
   return (
-    <div className="card">
+    <div className="card" key={pokeKey}>
       <h3>{pokeName.charAt(0).toUpperCase() + pokeName.slice(1)}</h3>
       <img src={pokemon} alt={pokeName + " sprite"} />
     </div>
