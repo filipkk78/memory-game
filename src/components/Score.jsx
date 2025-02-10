ebdb6c83dc273453443856ff4de2bb614ffcdd48
filipkk78 <filipkk78@gmail.com>
2025-02-10 17:12:48 +0100

    Update score display

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "../styles/Score.css";
 
-function Score() {
-  const [score, setScore] = useState(0);
-  const [highScore, setHighScore] = useState(0);
+function Score({ score, highScore }) {
   return (
     <div className="score">
       <p>Current: {score}</p>
